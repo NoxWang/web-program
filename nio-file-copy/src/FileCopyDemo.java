51485f3f1770246b70519cc220404f17cbaeec9b
NoxWang <wyf9612@126.com>
2020-10-31 20:40:36 +0800

    文件拷贝修改

--- nio-file-copy/src/FileCopyDemo.java
+++ nio-file-copy/src/FileCopyDemo.java
@@ -2,6 +2,9 @@ import java.io.*;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 
+/**
+ * 定义接口
+ */
 interface FileCopyRunner {
     void copyFile(File source, File target);
 }
@@ -33,6 +36,9 @@ public class FileCopyDemo {
 
     public static void main(String[] args) {
 
+        /**
+         * 使用Stream，不使用缓冲区
+         */
         FileCopyRunner noBufferStreamCopy = new FileCopyRunner() {
             @Override
             public void copyFile(File source, File target) {
@@ -43,6 +49,7 @@ public class FileCopyDemo {
                     fout = new FileOutputStream(target);
 
                     int result;
+                    // 读到结尾时，会返回-1
                     while ((result = fin.read()) != -1) {
                         fout.write(result);
                     }
@@ -60,17 +67,23 @@ public class FileCopyDemo {
             }
         };
 
+        /**
+         * 使用Stream，使用缓冲区
+         */
         FileCopyRunner bufferedStreamCopy = new FileCopyRunner() {
             @Override
             public void copyFile(File source, File target) {
                 InputStream fin = null;
                 OutputStream fout = null;
                 try {
+                    // 在FileInputStream上包裹一个BufferedInputStream
                     fin = new BufferedInputStream(new FileInputStream(source));
                     fout = new BufferedOutputStream(new FileOutputStream(target));
 
+                    // 缓冲区
                     byte[] buffer = new byte[1024];
 
+                    // 没读到结尾时，result为这一次读到的数量；读到结尾时，result为-1
                     int result;
                     while ((result = fin.read(buffer)) != -1) {
                         fout.write(buffer, 0, result);
@@ -89,22 +102,32 @@ public class FileCopyDemo {
             }
         };
 
+        /**
+         * 使用 Channel，使用 Buffer 进行读写
+         */
         FileCopyRunner nioBufferCopy = new FileCopyRunner() {
             @Override
             public void copyFile(File source, File target) {
+                // 声明文件通道
                 FileChannel fin = null;
                 FileChannel fout = null;
 
                 try {
+                    // 通过文件输入输出流得到文件通道
                     fin = new FileInputStream(source).getChannel();
                     fout = new FileOutputStream(target).getChannel();
 
+                    // 创建ByteBuffer类型的缓冲区（按字节读取）
                     ByteBuffer buffer = ByteBuffer.allocate(1024);
+                    // 将数据从文件通道中读取出来，写进Buffer
                     while (fin.read(buffer) != -1) {
+                        // 将Buffer从写模式转换为读模式
                         buffer.flip();
                         while (buffer.hasRemaining()) { // 确保Buffer中的内容被读完
+                            // 将Buffer中的数据写入文件通道
                             fout.write(buffer);
                         }
+                        // 将Buffer从读模式转换为写模式
                         buffer.clear();
                     }
                 } catch (IOException e) {
@@ -121,19 +144,26 @@ public class FileCopyDemo {
             }
         };
 
+        /**
+         * 使用 Channel，在两个 Channel 间直接传输数据
+         */
         FileCopyRunner nioTransferCopy = new FileCopyRunner() {
             @Override
             public void copyFile(File source, File target) {
+                // 声明文件通道
                 FileChannel fin = null;
                 FileChannel fout = null;
 
                 try {
+                    // 通过输入输出流创建通道
                     fin = new FileInputStream(source).getChannel();
                     fout = new FileOutputStream(target).getChannel();
 
                     long transferred = 0L;
                     long size = fin.size();
                     while (transferred != size) {
+                        // 从位置0开始，拷贝fin通道中size长度的数据，至fout通道，返回的是已拷贝长度
+                        // transferTo不能保证拷贝通道中的所有数据，因此使用while循环
                         transferred += fin.transferTo(0, size, fout);
                     }
 
